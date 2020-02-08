42be204343b5739d242001489ce34bd6bd78aa34
nsengar <nsengar@mninsengar.modeln.com>
2020-02-08 22:14:11 +0530

    Added comment to provide code information

--- Miscellaneous Coding Practice/src/com/java/century_from_year/CenturyFromYear.java
+++ Miscellaneous Coding Practice/src/com/java/century_from_year/CenturyFromYear.java
@@ -1,5 +1,21 @@
 package com.java.century_from_year;
 
+
+/*
+ * Given a year, return the century it is in. 
+ * The first century spans from the year 1 up to and including the year 100,
+ * the second - from the year 101 up to and including the year 200, etc.
+
+ *	Example
+	
+ *  For year = 1905, the output should be
+ *  centuryFromYear(year) = 20;
+	
+ *	For year = 1700, the output should be
+ *	centuryFromYear(year) = 17.
+
+*/
+
 public class CenturyFromYear {
 
 	public static void main(String[] args) {
@@ -8,9 +24,14 @@ public class CenturyFromYear {
 
 	private static int centuryFromYear(int year) {
 		
+		//Checking the condition that the year should be between (1,2005)
 		if(year>=1 && year<=2005 && year%100==0) {
+			
+			//if year%100 is 0 then return the year/100
 			return (year/100);
 		}else {
+			
+			//if (year % 100) is not 0 then return ((year/100 + 1)
 			return ((year/100)+1);
 		}
 	}
