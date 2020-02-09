01565e158bef25fbfed7323592a937c1cffd9652
nsengar <nsengar@mninsengar.modeln.com>
2020-02-09 23:15:12 +0530

    Initial commit...

--- Miscellaneous Coding Practice/src/com/java/adjacent_elements_product/AdjacentElementsProduct.java
+++ Miscellaneous Coding Practice/src/com/java/adjacent_elements_product/AdjacentElementsProduct.java
@@ -3,19 +3,28 @@ package com.java.adjacent_elements_product;
 import java.util.ArrayList;
 import java.util.Collections;
 
+//Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
 public class AdjacentElementsProduct {
 
 	public static int adjacentElementsProduct(int[] inputArray) {
 		
+		//taking a variable to store the product of adjacent elements.
 		int adjacentElementsProduct=0;
 		
-		ArrayList<Integer> list=new ArrayList<Integer>();
+		//ArrayList to store the product of all adjacent elements.
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
 		
 		for(int i=0; i<inputArray.length-1; i++) {
+			
+			//Storing the product
 			adjacentElementsProduct = inputArray[i] * inputArray[i+1];
+			
+			//adding the product to the list
 			list.add(adjacentElementsProduct);
 		}
 		
+		//return the max value of the product.
 		return Collections.max(list);
 	}
 	
