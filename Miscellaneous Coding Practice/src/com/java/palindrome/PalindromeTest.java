50f407b929bf2284b9e5fa30b23beb61b19d392c
nsengar <nsengar@mninsengar.modeln.com>
2020-02-09 09:55:16 +0530

    Added comments to provide the description of the program.

--- Miscellaneous Coding Practice/src/com/java/palindrome/PalindromeTest.java
+++ Miscellaneous Coding Practice/src/com/java/palindrome/PalindromeTest.java
@@ -1,17 +1,26 @@
 package com.java.palindrome;
 
+
+//https://github.com/nikkisengar/Miscellaneous-Coding-Practice.git
 public class PalindromeTest {
 
 	public static boolean checkPalindrome(String inputString) {
 		
+		//taking an empty string to store the reverse of the input String.
 		String reverseString = "";
 		
+		//Constraints
 		if(inputString.length()>=1 && inputString.length()<=Math.pow(10, 5)) {
+			
+			
 			for(int i=(inputString.length()-1); i>=0; i--) {
+				
+				//Reversing the input string and storing it into the reverseString variable
 				reverseString = reverseString +inputString.charAt(i);
 			}
 		}
 		
+		//checking if the inputString and the reverseString are equal or not
 		return (inputString.equals(reverseString));
 	}
 	
