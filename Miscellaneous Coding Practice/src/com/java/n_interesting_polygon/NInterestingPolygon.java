d3dfe3a7dcda0108777a5b3ebde3bf754dde17fb
nsengar <nsengar@mninsengar.modeln.com>
2020-02-10 17:21:25 +0530

    Modified the logic to use normal multiplication instead of Math.pow() function

--- Miscellaneous Coding Practice/src/com/java/n_interesting_polygon/NInterestingPolygon.java
+++ Miscellaneous Coding Practice/src/com/java/n_interesting_polygon/NInterestingPolygon.java
@@ -31,7 +31,7 @@ public class NInterestingPolygon {
 		
 		if(valueOfN>=1 && valueOfN<=Math.pow(10, 4)) {
 			
-			return ((int)Math.pow(valueOfN, 2) + (int)Math.pow((valueOfN-1), 2));
+			return ((valueOfN * valueOfN) + (valueOfN-1) * (valueOfN -1));
 		}
 		else {
 			return 0;
