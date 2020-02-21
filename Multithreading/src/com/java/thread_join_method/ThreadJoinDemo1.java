f8ff8b47107012484c37999129168f1accbce94f
nsengar <nsengar@mninsengar.modeln.com>
2020-02-21 12:22:55 +0530

    initial commit...

---
+++ Multithreading/src/com/java/thread_join_method/ThreadJoinDemo1.java
@@ -0,0 +1,34 @@
+package com.java.thread_join_method;
+
+class MyJoinDemo extends Thread {
+
+	static Thread mt;
+
+	public void run() {
+		try {
+			mt.join();
+		} catch (InterruptedException e) {
+			System.out.println("Waiting for main Thread to complete...");
+		}
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Child Thread...");
+		}
+	}
+}
+
+public class ThreadJoinDemo1 {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		MyJoinDemo.mt = Thread.currentThread();
+
+		MyJoinDemo t = new MyJoinDemo();
+
+		t.start();
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Main Thread...");
+			Thread.sleep(2000);
+		}
+	}
+}
